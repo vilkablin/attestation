b84ae6699e1713703bc8dd2cdcf3d274a6846626
violetta_chernysheva <violetta_chernysheva@enot.work>
2025-05-22 15:20:05 +0300

    add title and favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
+    <title>Будет Чисто!</title>
+    <link rel="shortcut icon" href="icons/logo.svg" type="image/x-icon">
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     @inertiaHead
